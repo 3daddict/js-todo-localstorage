dd6c60482ddc0ef6f6addb98ed18c7393e528592
Michael Salvati <print@3daddict.com>
2018-10-30 13:29:35 -0700

    created add function

--- app.js
+++ app.js
@@ -6,6 +6,35 @@ var removeButton = document.getElementById('remove');
 
 addButton.addEventListener('click', () => {
     console.log('add button clicked');
+    var input = document.getElementById('input');
+    var item = input.value;
+
+    ul = document.getElementById('list');
+    var textnode = document.createTextNode(item);
+
+    if (item === '') {
+        return false;
+    } else {
+        li = document.createElement('li');
+        var checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.setAttribute('id', 'checked');
+
+        var label = document.createElement('label');
+        label.setAttribute('for', 'item');
+
+        ul.appendChild(label);
+        li.appendChild(checkbox);
+        label.appendChild(textnode);
+        li.appendChild(label);
+        ul.insertBefore(li, ul.childNodes[0]);
+        
+        setTimeout(() => {
+            li.className = 'visual';
+        }, 1);
+
+        input.value = '';
+    }
 });
 
 removeButton.addEventListener('click', () => {
